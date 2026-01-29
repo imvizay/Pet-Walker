d1581a2c4efbc5d11e0d6acfa42b4c70fbbe37fc
imvizay <vizzaymeena@gmail.com>
2026-01-29 19:37:10 +0530

    frontend folder structure created and a few things halted for keepings simple and understandable

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Routes,Route } from 'react-router-dom'
+function App() {
+  return (
+   <>
+   
+   <Routes path='/' element="">
+     <Route index element=""/>
+    
+   </Routes>
+
+   </>
+  )
+}
+
+export default App
